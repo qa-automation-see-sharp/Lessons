f669f1e2a0af1406f1175690e5cbe294c9111846
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-07-08 23:14:26 -0700

    Add String examples

--- Lesson_2/StringsAndChars/Program.cs
+++ Lesson_2/StringsAndChars/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// initialization
+string firstName;
+firstName = "Oleh";
+var lastName = "Kutafin";
 
-Console.WriteLine("Hello, World!");
+// concatenation
+var fullName = firstName + " " + lastName;
+
+// Read from the console example
+Console.WriteLine("Hi there user! What is your first name?");
+var firstName1 = Console.ReadLine();
+Console.WriteLine("Hi there user! What is your last name?");
+var lastName1 = Console.ReadLine();
+Console.WriteLine("How old are you?");
+var usersAge = Console.ReadLine();
+
+//What is Char?
+var whatIsThis = lastName1[0];
+var myChar = 'a';
+
+Console.WriteLine("Welcome to our service");
+Console.WriteLine("Full name: " + fullName);
+Console.WriteLine("Age: " + usersAge);
+
+Console.WriteLine("Your name has " + firstName1.Length + " characters");
+Console.WriteLine("And your name start with " + firstName1[0]);
