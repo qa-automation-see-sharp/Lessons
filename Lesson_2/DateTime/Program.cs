a8df43c687bc3c68fbd34c4118479fa300549c1d
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-07-14 23:32:48 -0700

    add DateTime examples

--- Lesson_2/DateTime/Program.cs
+++ Lesson_2/DateTime/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Date and time can be stored in a DateTime variable
+// Dates can be assigned to a DateOnly variable
+// Times can be assigned to a TimeOnly variable
 
-Console.WriteLine("Hello, World!");
+// we can declare a DateTime variable
+DateTime myDateTime;
+
+// we can declare a DateOnly variable
+DateOnly myDate;
+
+// we can declare a TimeOnly variable
+TimeOnly myTime;
+
+
+// We can assign a value to these variables
+myDateTime = DateTime.Now;
+myDate = new DateOnly(2024, 1, 23);
+myTime = new TimeOnly(1, 23, 45);
+
+// We can declare and assign in one line
+DateTime myDateTime2 = DateTime.Now;
+DateOnly myDate2 = new DateOnly(2024, 1, 23);
+TimeOnly myTime2 = new TimeOnly(1, 23, 45);
+
+// We can re-assign a value to these variables
+myDateTime = DateTime.Now;
+myDate = new DateOnly(2024, 1, 23);
+myTime = new TimeOnly(1, 23, 45);
+
+// Dates and times are complex... consider
+// that we haven't factored in time zones!
+
+// we can make a DateTime variable out of
+// a DateOnly and a TimeOnly variable
+DateTime dateTimeFromCombination = new DateTime(
+    myDate,
+    myTime);
+
+// Let's write these to the console!
+Console.WriteLine($"Date Only: {myDate}");
+Console.WriteLine($"Time Only: {myTime}");
+Console.WriteLine($"Date Time: {dateTimeFromCombination}");
