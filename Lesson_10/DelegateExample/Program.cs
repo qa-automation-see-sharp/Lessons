396a8b8f3193463690dd051808ec604903275ec9
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-08-11 21:57:23 -0700

    lesson 9 and 10 draft

---
+++ Lesson_10/DelegateExample/Program.cs
@@ -0,0 +1,29 @@
+﻿var myClass = new MyClass();
+var myDelegate = new MyDelegate(myClass.Method1);
+var myDelegate2 = new MyDelegate(myClass.Method2);
+
+var both = myDelegate + myDelegate2;
+    both("Hello, from delegate");
+
+var anonimusDelegate = new MyDelegate(delegate { Console.WriteLine("Hello, from anonimus delegate"); });
+
+
+anonimusDelegate.Invoke(null);
+
+
+//TODO лямбда-вирази 
+
+public delegate void MyDelegate(string? message);
+
+public class MyClass
+{
+    public void Method1(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Method2(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
---
+++ Lesson_10/TaskExample/Program.cs
@@ -0,0 +1,33 @@
+﻿Task myTask = new Task(() => Console.WriteLine("Hello, from task"));
+myTask.Start();
+
+var myClass = new MyClass();
+    myClass.RunJob("Digging");
+
+Task<int> myTask2 = new Task<int>(() => Sum(5, 6));
+myTask2.Start();
+var result = myTask2;
+Console.WriteLine(result);
+
+
+
+
+int Sum(int a, int b) => a + b;
+
+
+public class MyClass
+{
+    public async Task RunJob(string managerMessage)
+    {
+        await Task.Run(() => { DoJob(managerMessage); });
+
+        Console.WriteLine($"Manager:Good Job!");
+    }
+
+    public async Task DoJob(string message)
+    {
+        Console.WriteLine($"Worker:Job {message} is being done...");
+        await Task.Delay(1000);
+        Console.WriteLine($"Worker:Job {message} is done!");
+    }
+}
